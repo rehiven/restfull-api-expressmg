14fa9dc4e3f5a87b4d0f0db695fa92e86bea256e
rehiven <cvargas199516@hotmail.com>
2019-12-01 09:45:06 -0600

    Mongodb soporte Agregado, GET para todos los libros, GET por genero, GET por Id

--- app.js
+++ app.js
@@ -1,14 +1,56 @@
-var express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
+const db = mongoose.connect("mongodb://localhost/bookAPI");
 
-var app = express();
+const app = express();
+const port = process.env.PORT || 3000;
+const bookRouter = express.Router();
+const bookGenreRouter = express.Router();
+const Book = require("./models/bookModel");
 
-var port = process.env.PORT || 3000;
 
-app.get('/', (req, res)=>{
-    res.send('Welcome to my API!!')
+//Filtra todos
+bookRouter.route("/books").get((req, res) => {
+  Book.find((err, books) => {
+    if (err) {
+      return res.send(err);
+    }
+    return res.json(books);
+  });
 });
 
-app.listen(port, ()=>{
-    console.log('Running on port '+ port);
+//Filtro por Id
+bookRouter.route("/books/:bookId").get((req, res) => {
+  Book.findById(req.params.bookId, (err, book) => {
+    if (err) {
+      return res.send(err);
+    }
+    return res.json(book);
+  });
 });
 
+//Filtra por genero
+bookGenreRouter.route("/booksGenre").get((req, res) => {
+  const query = {};
+  if (req.query.genre) {
+    query.genre = req.query.genre;
+  }
+  Book.find(query, (err, books) => {
+    if (err) {
+      return res.send(err);
+    }
+    return res.json(books);
+  });
+});
+
+app.use("/api", bookRouter);
+
+app.use("/api", bookGenreRouter);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to my API!!");
+});
+
+app.listen(port, () => {
+  console.log(`Running on port ${port}`);
+});
---
+++ booksJson.js
@@ -0,0 +1,44 @@
+db.books.insert([
+    {
+        title: "War and Peace",
+        genre: "Historical Fiction",
+        autor: "Lev Nikola Tolstoy",
+        read: false
+    },
+    {
+        title: "Les Miserables",
+        genre: "Historical Fiction",
+        autor: "Victor Hugo",
+        read: false
+    },
+    {
+        title: "The time Machine",
+        genre: "Science Fiction",
+        autor: "H. H. Wells",
+        read: false
+    },
+    {
+        title: "A Journey into the Center of the Earth",
+        genre: "Science Fiction",
+        autor: "Jules Verne",
+        read: false
+    },
+    {
+        title: "The Dark World",
+        genre: "Fantasy",
+        autor: "Henry Kuttner",
+        read: false
+    },
+    {
+        title: "The Duck History",
+        genre: "Historical Real",
+        autor: "V. A. Christopher",
+        read: false
+    },
+    {
+        title: "The Wind in the Willows",
+        genre: "Fantasy",
+        autor: "Knneth Grahame",
+        read: false
+    }
+]);
