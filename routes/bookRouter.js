ae3397c2653f67b0094b28ec976d142959d82f1d
rehiven <cvargas199516@hotmail.com>
2019-12-02 15:56:48 -0600

    Implementacion de PUT, DELETE, PACH en router.js

--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -19,15 +19,64 @@ function routes(Book) {
       });
     });
 
-  //Filtro por Id
-  bookRouter.route("/books/:bookId").get((req, res) => {
+  //Middleware
+
+  bookRouter.use("/books/:bookId", (req, res, next) => {
     Book.findById(req.params.bookId, (err, book) => {
       if (err) {
         return res.send(err);
       }
-      return res.json(book);
+      if (book) {
+        req.book = book;
+        return next();
+      }
+      return res.sendStatus(404);
     });
   });
+
+  //Filtro por Id
+  bookRouter
+    .route("/books/:bookId")
+    .get((req, res) => {
+      res.json(req.book);
+    })
+    .put((req, res) => {
+      const { book } = req;
+      book.title = req.body.title;
+      book.author = req.body.author;
+      book.genre = req.body.genre;
+      book.read = req.body.read;
+      req.book.save(err => {
+        if (err) {
+          return res.send(err);
+        }
+        return res.json(book);
+      });
+    })
+    .patch((req, res) => {
+      const { book } = req;
+      if (req.body._id) {
+        delete req.body._id;
+      }
+      Object.entries(req.body).forEach(item => {
+        const key = item[0];
+        const value = item[1];
+        book[key] = value;
+      });
+      req.book.save(err => {
+        if (err) {
+          return res.send(err);
+        }
+        return res.json(book);
+      });
+    }).delete((req, res)=>{
+      req.book.remove((err)=>{
+        if (err) {
+          return res.send(err);
+        }
+        return res.sendStatus(204);
+      });
+    })
 
   //Filtra por genero
   bookRouter.route("/booksg").get((req, res) => {
